b7a4de9fb5fae06085f10854d73007e8968b5aec
amandaflote <amanda.flote@gmail.com>
2021-04-20 15:19:56 +0200

    Sensors

---
+++ app/src/main/java/com/example/nosnooze/Pedometer.java
@@ -0,0 +1,13 @@
+package com.example.nosnooze;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import static androidx.core.content.ContextCompat.getSystemService;
+
+public class Pedometer extends AppCompatActivity {
+
+}
