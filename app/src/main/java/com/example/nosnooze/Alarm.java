f49f14fbb9fbdaa7bd76882c305d74a2c1e5d860
samuel@alberius.se <samuel@alberius.se>
2021-04-22 20:48:11 +0200

    Can now add alarms, delete alarms, toggle activity on alarms, added styling, fixed time formatting bugs

---
+++ app/src/main/java/com/example/nosnooze/Alarm.java
@@ -0,0 +1,4 @@
+package com.example.nosnooze;
+
+public class Alarm {
+}
---
+++ app/src/main/java/com/example/nosnooze/AlarmReceiver.java
@@ -0,0 +1,22 @@
+package com.example.nosnooze;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class AlertReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Come on, come on\n" +
+                "I see no changes, wake up in the morning, and I ask myself\n" +
+                "Is life worth living, should I blast myself?\n" +
+                "I'm tired of bein' poor, and even worse I'm black\n" +
+                "My stomach hurts, so I'm lookin' for a purse to snatch", Toast.LENGTH_SHORT).show();
+        String get_your_string = intent.getExtras().getString("extra");
+        Intent serviceIntent = new Intent(context, RingtonePlayingService.class);
+        serviceIntent.putExtra("extra", get_your_string);
+        context.startService(serviceIntent);
+    }
+}
